3ff67269a3aecab932c342b8e7cce84b14a188cd
shangqj <shangqj@si-tech.com.cn>
2024-01-30 14:54:10 +0800

    基础框架搭建

---
+++ src/main/java/com/docker/Main.java
@@ -0,0 +1,13 @@
+package com.docker;
+
+/**
+ * @description:
+ * @author: shangqj
+ * @date: 2024/1/19
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/docker/RedisDemo.java
@@ -0,0 +1,10 @@
+package com.docker;
+
+/**
+ * @description:
+ * @author: shangqj
+ * @date: 2024/1/19
+ * @version: 1.0
+ */
+public class RedisDemo {
+}
