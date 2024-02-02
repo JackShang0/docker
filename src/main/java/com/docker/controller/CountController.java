b8556e4c4f255292cb4adf2087acc8e95738423b
shangqj <shangqj@si-tech.com.cn>
2024-02-02 18:12:27 +0800

    调整

--- src/main/java/com/docker/controller/CountController.java
+++ src/main/java/com/docker/controller/CountController.java
@@ -17,9 +17,14 @@ public class CountController {
     @Autowired
     StringRedisTemplate redisTemplate;
 
+
+    /**
+     * docker集成redis 并操作成功
+     * url:<a href="http://47.106.68.235:6665/hello">...</a>
+     * @return 操作次数
+     */
     @GetMapping("/hello")
     public String count(){
-
         Long increment = redisTemplate.opsForValue().increment("count-hello",1);
         return "有 "+increment+" 人访问了 hello 页面";
     }
