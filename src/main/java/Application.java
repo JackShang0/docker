6c1727388039651b3607a3cde8709f9305685570
shangqj <shangqj@si-tech.com.cn>
2024-02-02 18:10:41 +0800

    docker操作redis案例

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,12 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication(scanBasePackages = {"com.docker"})
+@SpringBootApplication(scanBasePackages = {"com.docker"},exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+//@SpringBootApplication()
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
